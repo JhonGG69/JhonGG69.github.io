c282bbdfea87a3f1247144c19905d7afdc23989f
Jhon Gomez <jsebas.gomez@hotmail.com>
2025-04-14 19:44:37 -0500

    verificacion

--- api/config/src/backend/registro_propietario.js
+++ api/config/src/backend/registro_propietario.js
@@ -4,6 +4,7 @@ import bcrypt from "https://esm.sh/bcryptjs";
 // Conectar con Supabase
 const SUPABASE_URL = "@@SUPABASE_URL@@";
 const SUPABASE_ANON_KEY = "@@SUPABASE_ANON_KEY@@";
+console.log("SUPABASE_URL:", SUPABASE_URL);
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 document.addEventListener("DOMContentLoaded", function () {
